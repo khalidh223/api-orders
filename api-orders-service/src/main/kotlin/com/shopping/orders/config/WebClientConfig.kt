acc4ed7fab0f3c148b5e40df009a34fb31e3c352
Khalid Hussain <khalid.hussain@foodsby.com>
2022-10-15 00:41:26 -0500

    '#9: Adds Spring WebClient call to api-inventory before placing an order to check if the product is in stock/is available'

---
+++ api-orders-service/src/main/kotlin/com/shopping/orders/config/WebClientConfig.kt
@@ -0,0 +1,15 @@
+package com.shopping.orders.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.client.WebClient
+
+@Configuration
+class WebClientConfig {
+
+    @Bean
+    fun webClient(): WebClient {
+        return WebClient.builder().build()
+    }
+
+}
